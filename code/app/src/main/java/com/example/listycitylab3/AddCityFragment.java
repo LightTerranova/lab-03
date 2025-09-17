c305fb7ce1f0c7f3996d9e76445864e547399e96
LightTerranova <dudiy@ualberta.ca>
2025-09-17 14:58:39 -0600

    Updates from Lab 3

---
+++ code/app/src/main/java/com/example/listycitylab3/AddCityFragment.java
@@ -0,0 +1,48 @@
+package com.example.listycitylab3;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class AddCityFragment extends DialogFragment {
+    interface AddCityDialogListener { // Passes the input back to Main Activity
+        void addCity(City city);
+    }
+    private AddCityDialogListener listener; // Need to save what activity is calling us
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context); // We want on attach to do everything it did, but add more functionality below
+        if (context instanceof AddCityDialogListener) {
+            listener = (AddCityDialogListener) context; // We are casting the context to our interface now our listener is our interface
+        } else {
+            throw new RuntimeException(context + " must implement AddCityDialogListener"); // Cant work with an activity that doesnt implement our interface
+        }
+    }
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view =
+                LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_city, null);
+        EditText editCityName = view.findViewById(R.id.edit_text_city_text);
+        EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder // functions return a reference to the builder
+                .setView(view)
+                .setTitle("Add a city")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Add", (dialog, which) -> {
+                    String cityName = editCityName.getText().toString();
+                    String provinceName = editProvinceName.getText().toString();
+                    listener.addCity(new City(cityName, provinceName));
+                })
+                .create();
+    }
+}
